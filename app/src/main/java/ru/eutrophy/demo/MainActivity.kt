a2c1107e6d6be611eb84bd3b5491aed11c9cdcb3
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-11 13:54:53 +0300

    add user center and check permissions

--- app/src/main/java/ru/eutrophy/demo/MainActivity.kt
+++ app/src/main/java/ru/eutrophy/demo/MainActivity.kt
@@ -1,26 +1,44 @@
 package ru.eutrophy.demo
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color.GRAY
+import android.graphics.PointF
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat.checkSelfPermission
 import com.yandex.mapkit.Animation
+import com.yandex.mapkit.Animation.Type.SMOOTH
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.ObjectEvent
 import com.yandex.mapkit.logo.Alignment
-import com.yandex.mapkit.logo.HorizontalAlignment
-import com.yandex.mapkit.logo.VerticalAlignment
+import com.yandex.mapkit.logo.HorizontalAlignment.LEFT
+import com.yandex.mapkit.logo.VerticalAlignment.BOTTOM
+import com.yandex.mapkit.map.CameraListener
 import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.MapType
+import com.yandex.mapkit.map.CameraUpdateSource
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.image.ImageProvider.fromResource
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), UserLocationObjectListener, CameraListener {
 
     companion object {
-        const val MAP_KEY = "You key"
+        const val MAP_KEY = "YouKEY"
+        const val requestPermissionLocation = 1
     }
+
+    private lateinit var userLocationLayer: UserLocationLayer
+
+    private var routeStartLocation = Point(0.0, 0.0)
+
+    private var permissionLocation = false
+    private var followUserLocation = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,63 +47,139 @@ class MainActivity : AppCompatActivity() {
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
 
-        val map = mapview.map
-        map.mapType = MapType.VECTOR_MAP
+        checkPermission()
 
-        // Запрещено скрывать лого яндекса
-        mapview.map.logo.setAlignment(Alignment(HorizontalAlignment.RIGHT, VerticalAlignment.TOP))
+        userInterface()
 
-        try {
-            map.setMapStyle(style())
-        } catch (e: IOException) {
-            Log.e("ABAY", "Failed to read customization style", e);
-        }
 
-        mapview.map.move(
-            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 0f), null
-        )
+        /*     val map = map_v.map
+             map.mapType = MapType.VECTOR_MAP
+
+             // Запрещено скрывать лого яндекса
+             map_v.map.logo.setAlignment(Alignment(HorizontalAlignment.RIGHT, VerticalAlignment.TOP))
+
+             try {
+                 map.setMapStyle(style())
+             } catch (e: IOException) {
+                 Log.e("ABAY", "Failed to read customization style", e);
+             }
+
+             map_v.map.move(
+                 CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                 Animation(Animation.Type.SMOOTH, 0f), null
+             )*/
     }
 
-    @Throws(IOException::class)
-    private fun readRawResource(name: String): String? {
-        val builder = StringBuilder()
-        val resourceIdentifier = resources.getIdentifier(name, "raw", packageName)
-        val `is` = resources.openRawResource(resourceIdentifier)
-        val reader = BufferedReader(InputStreamReader(`is`))
-        try {
-            var line: String?
-            while (reader.readLine().also { line = it } != null) {
-                builder.append(line)
+
+    private fun checkPermission() {
+        val permissionLocation = checkSelfPermission(this, ACCESS_FINE_LOCATION)
+        if (permissionLocation != PERMISSION_GRANTED) {
+            requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), requestPermissionLocation)
+        } else {
+            onMapReady()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            requestPermissionLocation -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED) {
+                    onMapReady()
+                }
+                return
             }
-        } catch (ex: IOException) {
-            Log.e(
-                "ABAY",
-                "Cannot read raw resource $name"
-            )
-            throw ex
-        } finally {
-            reader.close()
         }
-        return builder.toString()
     }
 
+    private fun userInterface() {
+        val mapLogoAlignment = Alignment(LEFT, BOTTOM)
+        map_v.map.logo.setAlignment(mapLogoAlignment)
 
-    private fun style(): String {
-        return readRawResource("map_style") ?: ""
+        user_location_fab.setOnClickListener {
+            if (permissionLocation) {
+                cameraUserPosition()
+
+                followUserLocation = true
+            } else {
+                checkPermission()
+            }
+        }
     }
 
-    /* Передайте события onStart и onStop в MapKitFactory и mapView. Иначе MapKit не сможет
-    * отобразить карту и остановить обработку карты, когда Activity с картой становится невидимым для пользователя:*/
+    private fun onMapReady() {
+        val mapKit = MapKitFactory.getInstance()
+        userLocationLayer = mapKit.createUserLocationLayer(map_v.mapWindow)
+        userLocationLayer.isVisible = true
+        userLocationLayer.isHeadingEnabled = true
+        userLocationLayer.setObjectListener(this)
+
+        map_v.map.addCameraListener(this)
+
+        cameraUserPosition()
+
+        permissionLocation = true
+    }
+
+    private fun cameraUserPosition() {
+        if (userLocationLayer.cameraPosition() != null) {
+            routeStartLocation = userLocationLayer.cameraPosition()!!.target
+            map_v.map.move(
+                CameraPosition(routeStartLocation, 16f, 0f, 0f), Animation(SMOOTH, 1f), null
+            )
+        } else {
+            map_v.map.move(CameraPosition(Point(0.0, 0.0), 16f, 0f, 0f))
+        }
+    }
+
+    override fun onCameraPositionChanged(p0: Map, p1: CameraPosition, p2: CameraUpdateSource, finish: Boolean) {
+        if (finish) {
+            if (followUserLocation) {
+                setAnchor()
+            }
+        } else { if (!followUserLocation) { noAnchor() }
+        }
+    }
+
+    private fun setAnchor() {
+        userLocationLayer.setAnchor(
+            PointF((map_v.width * 0.5).toFloat(), (map_v.height * 0.5).toFloat()),
+            PointF((map_v.width * 0.5).toFloat(), (map_v.height * 0.83).toFloat())
+        )
+
+        user_location_fab.setImageResource(R.drawable.ic_baseline_gps)
+
+        followUserLocation = false
+    }
+
+    private fun noAnchor() {
+        userLocationLayer.resetAnchor()
+
+        user_location_fab.setImageResource(R.drawable.ic_baseline_add_24)
+    }
+
+    override fun onObjectAdded(userLocationView: UserLocationView) {
+       setAnchor()
+
+        userLocationView.pin.setIcon(fromResource(this, R.drawable.ic_baseline_add_24))
+        userLocationView.arrow.setIcon(fromResource(this, R.drawable.ic_baseline_gps))
+        userLocationView.accuracyCircle.fillColor = GRAY
+    }
+
+    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {}
+
+    override fun onObjectRemoved(p0: UserLocationView) {}
+
     override fun onStop() {
-        super.onStop()
-        mapview.onStop()
+        map_v.onStop()
         MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 
     override fun onStart() {
         super.onStart()
-        mapview.onStart()
         MapKitFactory.getInstance().onStart()
+        map_v.onStart()
     }
+
 }
